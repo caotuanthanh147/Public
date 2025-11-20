3086a8eb469946e33ee79de4c5a01e875e49290b
caotuanthanh147 <136108190+caotuanthanh147@users.noreply.github.com>
2025-11-20 16:24:33 +0700

    Add files via upload

---
+++ writestatus.lua
@@ -0,0 +1,20 @@
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local userId = tostring(Players.LocalPlayer.UserId)
+local function writeStatus(status)
+    local data = { status = status, timestamp = os.time() }
+    local jsonText = HttpService:JSONEncode(data)
+        writefile(userId .. ".status", jsonText)
+end
+writeStatus("online")
+spawn(function()
+    while true do
+        wait(120)  
+        writeStatus("online")
+    end
+end)
+if game:IsA("DataModel") then
+    game:BindToClose(function()
+        writeStatus("disconnected")
+    end)
+end
